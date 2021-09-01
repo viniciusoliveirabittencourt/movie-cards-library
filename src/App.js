129a93e6bcf409ab1de2de4d7c71c7feb1c6a08f
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-09-01 18:45:04 -0300

    requisito 11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Rating from './components/Rating0';
 import Movies from './data';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList movies={ Movies } />
+      <Rating />
     </div>
   );
 }
