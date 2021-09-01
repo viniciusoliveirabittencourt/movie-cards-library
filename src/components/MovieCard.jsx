46d5566a5ca4570c76494ae2c713336ff0324547
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-09-01 18:42:33 -0300

    requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,7 @@ class MovieCard extends React.Component {
         <section>
             <img src={ imagePath } alt={`Imagem do filme ${title}`} />
             <h4>{ title }</h4>
+            <h5>{ subtitle }</h5>
         </section>
     );
   }
